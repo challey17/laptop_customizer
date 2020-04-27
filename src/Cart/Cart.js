676b9be37b1e773986742621b8a2a94ae89115b7
Chayce Knaub <chknaub@yahoo.com>
2020-04-27 16:59:48 -0400

    cleaned up code

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -12,25 +12,6 @@ const USCurrencyFormat = new Intl.NumberFormat("en-US", {
 
 class Cart extends Component {
   render() {
-    // Cart Component
-    ////FIGURE OUT WHAT PROPS NEED TO BE PASSED TO SUMMARY COMPONENT
-    // const summary = Object.keys(this.props.selectedFeatures).map(
-    //   (feature, idx) => {
-    //     const featureHash = feature + "-" + idx;
-    //     const selectedOption = this.props.selectedFeatures[feature];
-
-    //     return (
-    //       <div className="summary__option" key={featureHash}>
-    //         <div className="summary__option__label">{feature} </div>
-    //         <div className="summary__option__value">{selectedOption.name}</div>
-    //         <div className="summary__option__cost">
-    //           {USCurrencyFormat.format(selectedOption.cost)}
-    //         </div>
-    //       </div>
-    //     );
-    //   }
-    // );
-
     return (
       <section className="main__summary">
         <Summary
